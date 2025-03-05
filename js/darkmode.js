116cedb6955daa82c36c1fcdb74cb5b8dc30d127
Emilie Thon <emilie.m.thon@gmail.com>
2025-03-05 11:16:18 +0100

    some corrections

--- js/darkmode.js
+++ js/darkmode.js
@@ -58,5 +58,3 @@ function switchTheme() {
     )
   }
 }
-
-console.log(movieIcon);
--- js/detail/detail.js
+++ js/detail/detail.js
@@ -1,22 +1,13 @@
+//! HEADER:
 let header = document.querySelector("header");
 header.innerHTML = `
 <nav class="header__nav">
-    <div class="header__menu">
-        <div class="header__burger">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-        <ul class="header__links">
-            <li class="header__link"><img src="./img/movieicon-light.svg" alt="Movie icon"></li>
-            <li class="header__link"><i class="fa-solid fa-ticket-simple"></i></li>
-            <li class="header__link"><i class="fa-solid fa-bookmark"></i></li>
-        </ul>
+    <div class="header__div">
+        <a href="index.html"><i class="fa-solid fa-arrow-left-long"></i></a>
+        <form action="#" class="header__switch" method="post">
+            <input type="checkbox" id="switch" value="false" name="switch">
+            <span class="slider round"></span>
+        </form>
     </div>
-    <h1>MyMovies</h1>
-    <form action="#" class="header__switch" method="post">
-        <input type="checkbox" id="switch" value="false" name="switch">
-        <span class="slider round"></span>
-    </form>
 </nav>
 `;
