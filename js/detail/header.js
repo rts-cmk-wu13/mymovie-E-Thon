d4fc6b8c388cb8dc8426b3c119876e4573a3c26f
Emilie Thon <emilie.m.thon@gmail.com>
2025-03-11 09:01:41 +0100

    video correction

--- js/detail/header.js
+++ js/detail/header.js
@@ -15,6 +15,9 @@ function header() {
       let header = document.querySelector("header");
       header.innerHTML = `
         <nav class="header__nav">
+          <div class="header__trailer">
+              <iframe src="https://www.youtube.com/embed/${movie.results[0]?.key}" allowfullscreen></iframe>
+          </div>
           <div class="header__div">
             <a href="index.html"><i class="fa-solid fa-arrow-left-long"></i></a>
             <form action="#" class="header__switch" method="post">
@@ -23,9 +26,6 @@ function header() {
             </form>
           </div>
         </nav>
-        <div class="header_trailer">
-            <iframe src="https://www.youtube.com/embed/${movie.results[0]?.key}" allowfullscreen></iframe>
-        </div>
 
 `;
 initDarkMode();
