f8b6d7413d5551e16b12e06db8904b9afda62eee
Emilie Thon <emilie.m.thon@gmail.com>
2025-03-11 08:31:45 +0100

    video embed added

--- js/detail/header.js
+++ js/detail/header.js
@@ -15,17 +15,18 @@ function header() {
       let header = document.querySelector("header");
       header.innerHTML = `
         <nav class="header__nav">
-            <div class="header_trailer">
-                <!-- <iframe src="https://www.youtube.com/embed/${movie.results[0]?.key}" allowfullscreen></iframe> -->
-            </div>
-            <div class="header__div">
-                <a href="index.html"><i class="fa-solid fa-arrow-left-long"></i></a>
-                <form action="#" class="header__switch" method="post">
-                    <input type="checkbox" id="switch" value="false" name="switch">
-                    <span class="slider round"></span>
-                </form>
-            </div>
+          <div class="header__div">
+            <a href="index.html"><i class="fa-solid fa-arrow-left-long"></i></a>
+            <form action="#" class="header__switch" method="post">
+              <input type="checkbox" id="switch" value="false" name="switch">
+              <span class="slider round"></span>
+            </form>
+          </div>
         </nav>
+        <div class="header_trailer">
+            <iframe src="https://www.youtube.com/embed/${movie.results[0]?.key}" allowfullscreen></iframe>
+        </div>
+
 `;
 initDarkMode();
     })
