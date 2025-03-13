bc4ad5d30c793fa579827ed663cf748523f7bebb
Emilie Thon <emilie.m.thon@gmail.com>
2025-03-13 09:21:42 +0100

    INDEX: popular; comments added

--- js/index/popular.js
+++ js/index/popular.js
@@ -4,40 +4,52 @@ function popular(movies) {
     // URL; API-URL der skal hentes data fra
     // mapObject; object, hvor data gemmes
     // key; API'ens data vi vil gemme
-    return fetch(url, optionsList)
-      .then((res) => res.json())
-      // laver svaret til JSON format (bliver JavaScript objekt)
-      .then((data) => {
-        if (Array.isArray(data[key])) {
-          // hvis data[key] er et array, så køres forEach:
-          data[key].forEach(item => 
-            mapObject[item.id] = item.name);
-        } else {
-          mapObject[data.id] = data[key];
-        }
-      })
-      .catch(err => {
-        alert("The movie is not available");
-        console.error(err);
-      });
+    return (
+      fetch(url, optionsList)
+        .then((res) => res.json())
+        // laver svaret til JSON format (bliver JavaScript objekt)
+        .then((data) => {
+          if (Array.isArray(data[key])) {
+            // hvis data[key] er et array, så køres forEach:
+            data[key].forEach((item) => (mapObject[item.id] = item.name));
+            // Hver genre gemmes i mapObject, hvor nøglen er item.id, og værdien er item.name
+          } else {
+            mapObject[data.id] = data[key];
+            // ellers, hvis det ikke er et array, så gemmes det direkte i i mapObject med data.id som nøgle
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+    );
   }
 
   //! GENRE:
-  let genreMap = {}; // Objekt til at gemme genrer fra id til navn
+  let genreMap = {}; // Objekt til at gemme genrer fra id (som nøgle) og navn (som id)
 
   // Hent genrer og start film-fetch
-  fetchData("https://api.themoviedb.org/3/genre/movie/list?language=en-US", genreMap, "genres")
-    .then(() => fetchMoviesPop(1));
+  fetchData(
+    "https://api.themoviedb.org/3/genre/movie/list?language=en-US",
+    genreMap,
+    "genres"
+  ).then(() => fetchMoviesPop(1));
+  // når genrer er hentet, kaldes filmene i popular
 
   //! RUNTIME:
-  let runtimeMap = {}; // Objekt til at gemme runtime fra movieId til runtime
+  let runtimeMap = {}; // Objekt til at gemme filmens ID som nøgle og runtime som værdi
 
   function fetchMovieRuntime(movieId) {
     if (runtimeMap[movieId] !== undefined) {
-      return Promise.resolve(runtimeMap[movieId]); // Returnér direkte, hvis vi allerede har det
+      // hvis vi allerede har runtime, får vi værdien direkte som et promise uden at kalde API'et igen:
+      return Promise.resolve(runtimeMap[movieId]);
+    } else {
+      // hvis ikke vi har runtime; fetchData kaldes og data gemmes i runtimeMap
+      return fetchData(
+        `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+        runtimeMap,
+        "runtime"
+      ).then(() => runtimeMap[movieId]); // Returnér runtime vi har fetched
     }
-    return fetchData(`https://api.themoviedb.org/3/movie/${movieId}?language=en-US`, runtimeMap, "runtime")
-      .then(() => runtimeMap[movieId] || 0); // Returnér runtime eller 0 ved fejl
   }
 
   //! PAGES COUNT:
@@ -49,7 +61,7 @@ function popular(movies) {
     entries.forEach(function (entry) {
       if (entry.isIntersecting) {
         currentOffsetPop++;
-        if (currentOffsetPop <= pages) { // Skiftet fra < til <=
+        if (currentOffsetPop <= pages) {
           fetchMoviesPop(currentOffsetPop);
         } else {
           console.log("There are no more movies for now");
@@ -73,9 +85,8 @@ function popular(movies) {
   divElm2.className = "movies2__div";
   sectionElm2.append(divElm2);
 
-  function fetchMoviesPop(offset) {
-    const urlPop = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${offset}`;
-    fetch(urlPop, optionsList)
+  function fetchMoviesPop() {
+    fetch(urlList, optionsList)
       .then((res) => res.json())
       .then((movies) => {
         // Hent alle filmdata og deres runtime samtidig
@@ -88,17 +99,15 @@ function popular(movies) {
           .then((moviesWithRuntime) => {
             divElm2.innerHTML += moviesWithRuntime
               .map((movie) => {
-
                 // billedsti:
-              let imageUrl;
-        
-              if (movie.poster_path) {
-                imageUrl = `https://image.tmdb.org/t/p/original/${
-                  movie.poster_path
-                }`;
-              } else {
-                imageUrl = 'https://placehold.co/300x450/transparent/000?text=N/A';
-              } 
+                let imageUrl;
+
+                if (movie.poster_path) {
+                  imageUrl = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+                } else {
+                  imageUrl =
+                    "https://placehold.co/300x450/transparent/000?text=N/A";
+                }
 
                 return `
               <div class="movies2__movie">
@@ -125,7 +134,9 @@ function popular(movies) {
                       </ul>
                       <p class="movie__duration duration">
                         <i class="fa-solid fa-clock"></i>
-                        ${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}min
+                        ${Math.floor(movie.runtime / 60)}h ${
+                  movie.runtime % 60
+                }min
                       </p>
                   </article>   
               </div>
@@ -134,7 +145,9 @@ function popular(movies) {
               .join("");
 
             //! movies being observed
-            let observedMovie = divElm2.querySelector(".movies2__movie:last-of-type");
+            let observedMovie = divElm2.querySelector(
+              ".movies2__movie:last-of-type"
+            );
             observerPop.observe(observedMovie);
           })
           .catch((err) => {
